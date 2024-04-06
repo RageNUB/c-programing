be9ad6e3f218038b1a6b85971f071f1f7e636208
RageNUB <jirimon39@gmail.com>
2024-04-06 18:46:31 +0600

    question 3 solved

--- Algorithm Mid Term/question_3.cpp
+++ Algorithm Mid Term/question_3.cpp
@@ -5,6 +5,8 @@ const int N = 1e3 + 7;
 int n, m;
 char a[N][N];
 bool visited[N][N];
+int cnt;
+int minimum = INT_MAX;
 vector<pair<int, int>> path = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
 bool isValid(int ci, int cj)
@@ -17,6 +19,7 @@ bool isValid(int ci, int cj)
 void dfs(int si, int sj)
 {
     visited[si][sj] = true;
+    cnt++;
     for (int i = 0; i < 4; i++)
     {
         int ci = si + path[i].first;
@@ -38,14 +41,22 @@ int main()
             cin >> a[i][j];
         }
     }
-    int si, sj, di, dj;
-    cin >> si >> sj >> di >> dj;
-    dfs(si, sj);
-
-    if (visited[di][dj])
-        cout << "YES" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (!visited[i][j] && a[i][j] == '.')
+            {
+                cnt = 0;
+                dfs(i, j);
+                minimum = min(cnt, minimum);
+            }
+        }
+    }
+    if (minimum == INT_MAX)
+        cout << "-1" << endl;
     else
-        cout << "NO" << endl;
+        cout << minimum << endl;
 
     return 0;
 }
