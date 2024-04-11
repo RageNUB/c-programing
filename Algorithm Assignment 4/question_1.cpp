6bebc813eca2d0b69cba745b07cbd8eec020f21e
RageNUB <jirimon39@gmail.com>
2024-04-11 15:48:41 +0600

    algo assignment 4 question 1 solved

--- Algorithm Assignment 4/question_1.cpp
+++ Algorithm Assignment 4/question_1.cpp
@@ -1,7 +1,49 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    
+    int testCase;
+    cin >> testCase;
+    while (testCase--)
+    {
+        int n, size;
+        cin >> n >> size;
+        int v[n];
+        int w[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> w[i];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+        }
+
+        int dp[n + 1][size + 1];
+        for (int i = 0; i <= n; i++)
+        {
+            for (int j = 0; j <= size; j++)
+            {
+                if (i == 0 || j == 0)
+                    dp[i][j] = 0;
+            }
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= size; j++)
+            {
+                if (w[i - 1] <= j)
+                {
+                    dp[i][j] = max(v[i - 1] + dp[i - 1][j - w[i - 1]], dp[i - 1][j]);
+                }
+                else
+                {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        cout << dp[n][size] << endl;
+    }
+
     return 0;
 }
