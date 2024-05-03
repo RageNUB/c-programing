409b6a6ab872a6c04b4c19e81ee5fd9f903afdc5
RageNUB <jirimon39@gmail.com>
2024-05-04 01:10:26 +0600

    G_7 solved

--- SWE Junior long contest 2024/G_7.cpp
+++ SWE Junior long contest 2024/G_7.cpp
@@ -1,7 +1,20 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    
+    int h, m, s;
+    char c;
+    string ampm;
+    cin >> h >> c >> m >> c >> s >> ampm;
+
+    if (ampm == "AM")
+    {
+        if (h == 12) h = 0;
+    }
+    else
+    {
+        if (h != 12) h += 12;
+    }
+    cout << setw(2) << setfill('0') << h << c << setw(2) << setfill('0') << m << c << setw(2) << setfill('0') << s << endl;
     return 0;
 }
