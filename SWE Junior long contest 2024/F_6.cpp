fa30ef2345010a711ec391bb834bb453086281cd
RageNUB <jirimon39@gmail.com>
2024-05-04 00:47:55 +0600

    F_6 solved

--- SWE Junior long contest 2024/F_6.cpp
+++ SWE Junior long contest 2024/F_6.cpp
@@ -1,7 +1,16 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    
+    string s;
+    cin >> s;
+    for (int i = 1; i <= s.length(); i++)
+    {
+        char tmp = s[(2 * i - 1) - 1];
+        s[(2 * i - 1) - 1] = s[(2 * i) - 1];
+        s[(2 * i) - 1] = tmp;
+    }
+    cout << s << endl;
+
     return 0;
 }
